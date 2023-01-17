c6b75f1bb2cb5d2276b953695f9369d396cb3b3c
Andrew Shearin <ashearin@usgs.gov>
2023-01-17 11:06:56 -0500

    added semicolon

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
-const functionApi = "https://fnapp-resume-m4hlhzfsttuqg.azurewebsites.net/api/counter/list?code=olCb69dj0A0RM-AEYSkrPfpfOKDUNPogcEW0Q7FKkG0gAzFuR6bNZg=="
+const functionApi = "https://fnapp-resume-m4hlhzfsttuqg.azurewebsites.net/api/counter/list?code=olCb69dj0A0RM-AEYSkrPfpfOKDUNPogcEW0Q7FKkG0gAzFuR6bNZg==";
 
 const getVisitCount = async () => {
     let count = 30;
